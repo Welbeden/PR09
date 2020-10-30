5ae1d2e04743bb6b46a65264cf102cbcc42249ce
Welbeden <43912308+Welbeden@users.noreply.github.com>
2020-10-30 11:46:11 +0300

    Add files via upload

--- RandWindow/Form1.cs
+++ RandWindow/Form1.cs
@@ -24,11 +24,8 @@ namespace RandWindow
     int _h = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;
 
     private void textBox2_MouseMove(object sender, MouseEventArgs e)
-        {
-
-      // ппереводим координату X в строку и записывает в поля ввода
+    {
       textBox1.Text = e.X.ToString();
-      // ппереводим координату Y в строку и записывает в поля ввода
       textBox2.Text = e.Y.ToString();
 
       if (e.X > 120 && e.X < 225 && e.Y > 320 && e.Y < 370)
@@ -49,27 +46,18 @@ namespace RandWindow
 
     private void button1_MouseClick(object sender, MouseEventArgs e)
     {
-      // Вывести сообщение с текстом "Вы усердны"
       MessageBox.Show("Вы усердны!!");
-      // Завершить приложение
       Application.Exit();
     }
 
     private void button2_MouseClick(object sender, MouseEventArgs e)
     {
-      // Вывести сообщение, с текстом "Мы не сомневались в вешем безразличии"
-      // второй параметр - заголовок окна сообщения "Внимание"
-      // MessageBoxButtons.OK - тип размещаемой кнопки на форме сообщения
-      // MessageBoxIcon.Information - тип сообщения - будет иметь иконку "информация" и соотвествующее звукововой сигнал
       MessageBox.Show("Мы не сомневались в вешем безразличии", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
     }
 
     private void Form1_MouseMove(object sender, MouseEventArgs e)
     {
-      
-      // ппереводим координату X в строку и записывает в поля ввода
       textBox1.Text = e.X.ToString();
-      // ппереводим координату Y в строку и записывает в поля ввода
       textBox2.Text = e.Y.ToString();
 
       if (e.X > 90 && e.X < 200 && e.Y > 190 && e.Y < 230)
